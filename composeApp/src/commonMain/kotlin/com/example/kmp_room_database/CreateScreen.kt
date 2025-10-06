a7c28644199ec96da262de6b3ff87e292448ffad
rezaulorangetoolz <rezaul.orangetoolz@gmail.com>
2025-10-06 11:05:05 +0600

    add text feild

--- composeApp/src/commonMain/kotlin/com/example/kmp_room_database/CreateScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_room_database/CreateScreen.kt
@@ -1,19 +1,29 @@
 package com.example.kmp_room_database
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun  CreateScreen(navController: NavHostController) {
+    val title = mutableStateOf("")
+    val content = mutableStateOf("")
     Scaffold (
         topBar = {
             TopAppBar(
@@ -30,8 +40,28 @@ fun  CreateScreen(navController: NavHostController) {
             );
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("Create Screen")
+        Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)) {
+            TextField(
+                value = title.value,
+                onValueChange = {it -> title.value = it},
+                label = {Text("Title")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            TextField(
+                value = content.value,
+                onValueChange = {it -> content.value = it},
+                label = {Text("Content")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                shape = RoundedCornerShape(5.dp)
+            ){
+                Text("Create")
+            }
         }
     }
 
--- composeApp/src/commonMain/kotlin/com/example/kmp_room_database/Icons.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_room_database/Icons.kt
@@ -1,6 +1,5 @@
 package com.example.kmp_room_database
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
